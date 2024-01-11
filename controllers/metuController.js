845d55c55c37a0de9ad5a393a5a259df2eb81e8e
cdalton316 <cdalton316@gmail.com>
2024-01-11 16:41:18 -0500

    more refactoring

--- controllers/metuController.js
+++ controllers/metuController.js
@@ -4,27 +4,42 @@ const Metu = require('./../models/metuModel');
 //   fs.readFileSync(`${__dirname}/../neteru-app/metutu/metutu.json`),
 // );
 
-exports.getAllMetutu = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // results: metutu.length,
-    // data: {
-    //   metutu,
-    // },
-  });
+exports.getAllMetutu = async (req, res) => {
+  try {
+    const metutu = await Metu.find();
+    res.status(200).json({
+      status: 'success',
+      results: metutu.length,
+      data: {
+        metutu,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getMetu = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
+exports.getMetu = async (req, res) => {
+  try {
+    const metu = await Metu.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        metu,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+  // console.log(req.params);
+  // const id = req.params.id * 1;
   // const metu = metutu.find((el) => el.id === id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     metu,
-  //   },
-  // });
 };
 exports.createMetu = async (req, res) => {
   try {
@@ -35,19 +50,43 @@ exports.createMetu = async (req, res) => {
         metutu: newMetu,
       },
     });
-  } catch (err) {}
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 };
-exports.updateMetu = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      metu: '<Update Metu here...>',
-    },
-  });
+exports.updateMetu = async (req, res) => {
+  try {
+    const metu = await Metu.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        metu,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.deleteMetu = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteMetu = async (req, res) => {
+  try {
+    await Metu.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
