84008b47bbc80fb11984d099b96aad0148b24e44
cdalton316 <cdalton316@gmail.com>
2024-01-12 01:56:32 +0000

    Metutu schema complete

--- models/metuModel.js
+++ models/metuModel.js
@@ -5,10 +5,38 @@ const metuSchema = new mongoose.Schema({
     required: [true, 'A metu must have a name'],
     unique: true,
   },
-  color: String,
-  day: String,
-  number: Number,
-  planet: String,
+  sphere: {
+    type: String,
+  },
+
+  color: { type: String },
+  day: { type: String },
+  number: { type: Number },
+  planet: { type: String },
+  gems: { type: String },
+  timeOfYear: { type: String },
+  esotericHerbalism: { type: String },
+  hekau: { type: String },
+  spiritualDirection: { type: String },
+  mundaneDirection: { type: String },
+  personalityPortfolio: {
+    type: String,
+    personalityTraits: String,
+    emotionalTraits: String,
+    mentalTraits: String,
+  },
+  socialCorrespondences: {
+    type: String,
+    careersFunctions: String,
+  },
+  spiritualPortfolio: { type: String },
+  keyPhrases: { type: String },
+  biologicalCorrespondences: { type: String },
+  pathology: { type: String },
+  kamiticTheraputics: { type: String },
+  chineeseMedice: { type: String },
+  spiritualkeynotes: { type: String },
+  spiritualCounsel: { type: String },
 });
 const Metu = mongoose.model('Metu', metuSchema);
 
