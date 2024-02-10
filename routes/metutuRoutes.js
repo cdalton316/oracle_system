f5ebe717861f496a7f9c02a92a10c57950abe45e
cdalton316 <cdalton316@gmail.com>
2024-02-09 19:09:18 -0500

    googles debuging

--- routes/metutuRoutes.js
+++ routes/metutuRoutes.js
@@ -5,7 +5,7 @@ const metuController = require('./../controllers/metuController');
 const router = express.Router();
 // router.param('id', metuController.checkID);
 
-router.route('/metu-stats').get(metuController.getMetuStats);
+// router.route('/metu-stats').get(metuController.getMetuStats);
 router
   .route('/')
   .get(metuController.getAllMetutu)
